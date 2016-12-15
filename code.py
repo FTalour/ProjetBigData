65db49e89dee32ea21a16b6f029368f916fca3ed
FTalour <florian.talour@u-psud.fr>
2016-12-15 18:19:35 +0100

    ajout de la fonction pour Mahalanobis Diagonale

--- code.py
+++ code.py
@@ -29,19 +29,27 @@ def precision(nbExemplesMalClasses, nbTotalExemples):
 def printErr(nbErreur, size):
 	print("Taux erreur : {0}".format(precision(nbErreur*100.0, size)), '%')
     
-# moyenne de chaque ligne
+# moyenne de chaque ligne avec un label
 def moyenne_X_lbl(x=X0, lbl=lbl0):
     moy = np.zeros((10, x.shape[1]))
     for i in range(0, 10):
         moy[i] = np.average(x[lbl == i,:], axis=0)
     return moy
     
-# standart deviation de chaque ligne
+# standart deviation de chaque ligne sans label
+def variance_X(x=X0):
+    variance = np.zeros(x.shape[1])
+    variance = np.var(x, axis=0)
+    return variance
+    
+# standart deviation de chaque ligne avec un label
 def variance_X_lbl(x=X0, lbl=lbl0):
     var = np.zeros((10, x.shape[1]))
     for i in range(0, 10):
         var[i] = np.var(x[lbl == i,:], axis=0)
     return var
+    
+    
 
 # 1PPV obtenir un sous-ensemble de la base d'apprentissage, on enlève p valeurs à x
 def trainPPV(x, p):
@@ -97,13 +105,13 @@ def predictDMINMahalanobisDiagonale(trainMat=X0, devMat=X1):
     resultat = np.zeros(devMat.shape[0], dtype = np.int)
 
     # calcul de l'ecart type de chaque nombre
-    variance = variance_X_lbl(trainMat)
+    variance = variance_X(trainMat)
     
     nberreur = 0
     # calcul du taux d'erreur
     for i in range(0, 10):
-        squareDst = np.subtract(devMat,moy[i])*np.subtract(devMat,moy[i])/variance[i]
-        dist[i] = np.sqrt(np.sum(squareDst, axis=1))
+        squareDst = np.subtract(devMat,moy[i])*np.subtract(devMat,moy[i])/variance
+        dist[i] = np.sum(squareDst, axis=1)
 
     resultat = np.argmin(dist, axis = 0)
     nberreur = sum(resultat != lbl1)
@@ -161,10 +169,10 @@ def supprVarianceInf(val=0, trainMat=X0, devMat=X1):
     
 def supprStdInf(val=0, trainMat=X0, devMat=X1):
     # varTrainMat contient les variances d'entrainement (784,)
-    varTrainMat =np.var(trainMat, axis=0)
+    varTrainMat =np.std(trainMat, axis=0)
     
     # varTrainMat contient les variances de developpement (784,)
-    varDevMat =np.var(devMat, axis = 0)
+    varDevMat =np.std(devMat, axis = 0)
     
     # tableaux des indices constants dans la matrice de developpement et d'entrainement
     trainKeep = varTrainMat>val
@@ -185,20 +193,6 @@ def supprStdInf(val=0, trainMat=X0, devMat=X1):
     
     return noConstTrainMat, noConstDevMat
 
-# Calcul de la distance de Mahalanobis entre x et moyenne sur x (ne marche pas)
-def dstMahalanobisDiagonale(x=X0):
-    D = np.zeros(x.shape[0])
-    mu = np.mean(x, axis=0)
-    variance = np.var(X0, axis = 0)
-    variance_inv = 1/variance
-    E = np.zeros((x.shape[1], x.shape[1]))
-    np.fill_diagonal(E, variance)
-    D = np.transpose(x-mu)*E*(x-mu)
-    #C = np.cov(x, rowvar=0)
-    #VI = np.linalg.inv(C) # numpy.linalg.linalg.LinAlgError: Singular matrix
-    #D = np.transpose(x-mu)*VI*(x-mu)
-    return D
-
 def confmat(true, pred):
     dim = max(pred)+1
     z = dim*true + pred
@@ -208,56 +202,49 @@ def confmat(true, pred):
     zcount_reshaped = zcount.reshape(dim,dim)
     
     zcount_reshaped_sumcolonne = np.sum(zcount_reshaped, axis=1)
+    
     zcount_reshaped_sumligne = np.sum(zcount_reshaped, axis=0)
     
     for i in range(0,zcount_reshaped_sumcolonne.shape[0]):
         # colonne (precision)
-        valeur_precision = np.diag(zcount_reshaped)/float(zcount_reshaped_sumcolonne[i])  
+        valeur_precision = zcount_reshaped/float(zcount_reshaped_sumcolonne[i])  
         # ligne (rappel)
-        valeur_rappel = np.diag(zcount_reshaped)/float(zcount_reshaped_sumligne[i])  
+        valeur_rappel = zcount_reshaped/float(zcount_reshaped_sumligne[i])  
     
     print("Matrice de confusion")
     print(zcount_reshaped)
 
+    # on arrondi les valeurs à l'affichage pour une meilleure lisibilitée
     print("Matrice de precision")
-    print(valeur_precision)
+    print(np.around(valeur_precision,3)) 
     
     print("Matrice de rappel")
-    print(valeur_rappel)
+    print(np.around(valeur_rappel,3))
 
+# décommenter les passages souhaités pour les executer
 def main():
-    
     '''
-    # naive_bayes
-    # sur X0
+    # naive_bayes avec ACP
     from sklearn.naive_bayes import GaussianNB
+    
+    #for i in range(5, 125, 10):
+    #    print i
     t1 = np.datetime64(dt.datetime.now())
     gnb = GaussianNB()
-    data = X0
-    target = lbl0
-    y_pred = gnb.fit(data, target).predict(data)
+    XRed0, XRed1, P = reduceMat(45, X0, X1)
+    print XRed0.shape
+    print XRed1.shape
+    y_pred = gnb.fit(XRed0, lbl0).predict(XRed1)
     t2 = np.datetime64(dt.datetime.now())
     print("Number of mislabeled points out of a total %d points : %d"
-            % (data.shape[0],(target != y_pred).sum()))
-    printErr((target != y_pred).sum(), data.shape[0])
-    print ("Temps Naive Bayes sans ACP sur X0 : ", t2 - t1)
-    confmat(target, y_pred)
-    
-    # sur X1
-    t1 = np.datetime64(dt.datetime.now())
-    data = X1
-    target = lbl1
-    y_pred = gnb.fit(data, target).predict(data)
-    print ("Temps Naive Bayes sans ACP : ", t2 - t1)
-    print("Number of mislabeled points out of a total %d points : %d"
-            % (data.shape[0],(target != y_pred).sum()))
-    printErr((target != y_pred).sum(), data.shape[0])
-    print ("Temps Naive Bayes sans ACP sur X1 : ", t2 - t1)
-    confmat(target, y_pred)
+            % (X1.shape[0],(lbl1 != y_pred).sum()))
+    printErr((lbl1 != y_pred).sum(), X1.shape[0])
+    print ("Temps Naive Bayes avec ACP : ", t2 - t1)
+    confmat(lbl1, y_pred)
     '''
 
     '''
-    # 1PPV (Mahalanobis diagonale) sans ACP
+    # 1PPV (KNeighborsClassifier) sans ACP
     from sklearn import neighbors
     t1 = np.datetime64(dt.datetime.now())
     n_neighbors = 15
@@ -271,9 +258,7 @@ def main():
     printErr((y != Z).sum(), X.shape[0])
     print ("Temps Mahalanobis diagonale sans ACP : ", t2 - t1)
     confmat(y, Z)
-    '''
     
-    '''
     # 1PPV (KNeighborsClassifier) avec ACP
     t1 = np.datetime64(dt.datetime.now())
     XRed0, XRed1, P = reduceMat(100, X0, X1)
@@ -296,7 +281,7 @@ def main():
     print X0.shape
     print X1.shape
     t1 = np.datetime64(dt.datetime.now())
-    res, prediction = predictDMIN(X0, X1)
+    precision, prediction = predictDMIN(X0, X1)
     t2 = np.datetime64(dt.datetime.now())
     print ("Temps DMIN sans ACP : ", t2 - t1)
     confmat(lbl1, prediction)
@@ -304,10 +289,10 @@ def main():
     # DMIN avec ACP
     print("DMIN avec ACP")
     t3 = np.datetime64(dt.datetime.now())
-    XRed0, XRed1, P = reduceMat(100, X0, X1)
+    XRed0, XRed1, P = reduceMat(90, X0, X1)
     print XRed0.shape
     print XRed1.shape
-    res, prediction = predictDMIN(XRed0, XRed1)
+    precision, prediction = predictDMIN(XRed0, XRed1)
     t4 = np.datetime64(dt.datetime.now())
     print ("Temps : ", t4 - t3)
     confmat(lbl1, prediction)
@@ -318,7 +303,7 @@ def main():
     noConstX0, noConstX1 = supprVarianceInf(0, X0, X1)
     print noConstX0.shape
     print noConstX1.shape
-    res, prediction = predictDMIN(noConstX0, noConstX1)
+    precision, prediction = predictDMIN(noConstX0, noConstX1)
     t6 = np.datetime64(dt.datetime.now())
     print ("Temps 1PPV en supprimant les valeurs constantes : ", t6 - t5)
     confmat(lbl1, prediction)
@@ -331,9 +316,22 @@ def main():
     noConstX0, noConstX1 = supprStdInf(0, X0, X1)
     print noConstX0.shape
     print noConstX1.shape
-    res, prediction = predictDMINMahalanobisDiagonale(noConstX0, noConstX1)
+    precision, prediction = predictDMINMahalanobisDiagonale(noConstX0, noConstX1)
     t6 = np.datetime64(dt.datetime.now())
-    print ("Temps 1PPV en supprimant les valeurs constantes : ", t6 - t5)
+    print ("Temps DMINMahalanobisDiagonale en supprimant les valeurs constantes : ", t6 - t5)
+    confmat(lbl1, prediction)
+    '''
+    
+    '''
+    # DMIN MahalanobisDiagonale avec ACP
+    print("DMINMahalanobisDiagonale avec ACP")
+    t5 = np.datetime64(dt.datetime.now())
+    XRed0, XRed1, P = reduceMat(90, X0, X1)
+    print XRed0.shape
+    print XRed1.shape
+    precision, prediction = predictDMINMahalanobisDiagonale(XRed0, XRed1)
+    t6 = np.datetime64(dt.datetime.now())
+    print ("Temps DMINMahalanobisDiagonale avec ACP : ", t6 - t5)
     confmat(lbl1, prediction)
     '''
     
@@ -344,7 +342,7 @@ def main():
     XRed0, XRed1, P = reduceMat(100, X0, X1)
     print XRed0.shape
     print XRed1.shape
-    res, prediction = predict1PPV(XRed0, XRed1)
+    precision, prediction = predict1PPV(XRed0, XRed1)
     t2 = np.datetime64(dt.datetime.now())
     print ("Temps 1PPV avec ACP : ", t2 - t1)
     confmat(lbl1, prediction)
@@ -352,7 +350,7 @@ def main():
     # 1PPV sans ACP (sans traitement)
     print("PPV sans ACP")
     t3 = np.datetime64(dt.datetime.now())
-    res, prediction = predict1PPV()
+    precision, prediction = predict1PPV()
     t4 = np.datetime64(dt.datetime.now())
     print ("Temps 1PPV sans ACP : ", t4 - t3)
     confmat(lbl1, prediction)
@@ -363,7 +361,7 @@ def main():
     noConstX0, noConstX1 = supprVarianceInf(0, X0, X1)
     print noConstX0.shape
     print noConstX1.shape
-    res, prediction = predict1PPV(noConstX0, noConstX1)
+    precision, prediction = predict1PPV(noConstX0, noConstX1)
     t6 = np.datetime64(dt.datetime.now())
     print ("Temps 1PPV en supprimant les valeurs constantes : ", t6 - t5)
     confmat(lbl1, prediction)
